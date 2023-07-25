a76c741f309e8594329e79deb25fc509811da546
Rahul <mtal3584@gmail.com>
2023-07-25 14:25:45 +0545

    User Authentication Functionality added

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("Home Page")),
+      body: Container(),
+    );
+  }
+}
